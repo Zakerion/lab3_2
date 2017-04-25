d8d0ea85c98eefd2839b4917e3f6e5d97ff2048c
Przyjemski.Maciej <180264@edu.p.lodz.pl>
2017-04-25 13:47:56 +0200

    dodanie getterów

--- src/main/java/edu/iis/mto/staticmock/PublishableNews.java
+++ src/main/java/edu/iis/mto/staticmock/PublishableNews.java
@@ -14,13 +14,21 @@ public class PublishableNews {
 
 	public void addPublicInfo(String content) {
 		this.publicContent.add(content);
-		
+
 	}
 
 	public void addForSubscription(String content, SubsciptionType subscriptionType) {
-		
+
 		this.subscribentContent.add(content);
-		
+
+	}
+
+	public List<String> getPublicContent() {
+		return publicContent;
+	}
+
+	public List<String> getSubscribentContent() {
+		return subscribentContent;
 	}
 
 }
